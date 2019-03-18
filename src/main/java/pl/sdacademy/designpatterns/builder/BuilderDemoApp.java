ebb4cc9e9a6d40ae4947a0a426ca027971007919
Michal Bojanowski <bojanowski.michal89@gmail.com>
2019-03-18 18:59:43 +0100

    Singleton 4 & Builder 1

---
+++ src/main/java/pl/sdacademy/designpatterns/builder/BuilderDemoApp.java
@@ -0,0 +1,23 @@
+package pl.sdacademy.designpatterns.builder;
+
+import java.util.Arrays;
+
+public class BuilderDemoApp {
+    public static void main(String[] args) {
+        final Player playerA = new Player.Builder()
+                .withHealth(100)
+                .withMana(50)
+                .withNick("Zerg")
+                .build();
+
+        final Player playerB = new Player.Builder()
+                .withFriends(Arrays.asList("Michal", "Adam"))
+                .withHealth(50)
+                .withNick("Ania")
+                .build();
+
+        // Add to String method
+        System.out.println(playerA);
+        System.out.println(playerB);
+    }
+}
