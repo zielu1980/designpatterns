fcd81bcee57e87dffc21e126370af5f8f4abb7da
Michal Bojanowski <bojanowski.michal89@gmail.com>
2019-03-19 18:36:47 +0100

    Adapter - 1

---
+++ src/main/java/pl/sdacademy/designpatterns/adapter/User.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.designpatterns.adapter;
+
+import java.util.List;
+
+public interface User {
+
+    String getFullname();
+    String getUsername();
+    Integer getAge();
+    List<String> getRoles();
+}
